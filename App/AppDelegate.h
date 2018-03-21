2dff12d073b34695afee7d9d0abb66bed5d09659
Ada 2018 <ada2018@172-16-1-99.0000.ifce.br.iosda.org>
2018-03-21 09:08:09 -0300

    Initial Commit

---
+++ App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by Ada 2018 on 21/03/18.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  App
+//
+//  Created by Ada 2018 on 21/03/18.
+//  Copyright © 2018 Ada 2018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
